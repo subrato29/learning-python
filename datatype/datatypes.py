3115c8d0d4ef5ec3d2ba689f304a5d372dc5cb82
subrato29 <subratosarkar29@gmai.com>
2022-07-01 12:16:34 -0400

    updated code

---
+++ datatype/datatypes.py
@@ -0,0 +1,26 @@
+a = 10
+print(a)
+
+if a >= 10:
+    print("Pass")
+
+a = "hello"
+print(a)
+
+a = 32 + 0.1
+print(a);
+
+b = a
+
+print(id(a))  # identity function- it returns identity id of a
+print(id(b))
+
+a, b, c = 3, 2, "string"
+print(a, b, c)
+
+s = "hello world"
+print(s[0])
+print(s[3:7])
+print(s[2:])
+print(s * 3)
+print(s + " python")
