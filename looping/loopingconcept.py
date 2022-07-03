91bdff13114ac040328926ebbb0a4daf95f10e82
subrato29 <subratosarkar29@gmai.com>
2022-07-03 10:27:27 -0400

    adding looping concept

---
+++ looping/loopingconcept.py
@@ -0,0 +1,36 @@
+# while loop
+count = 0;
+while count < 4:
+    print(count)
+    count = count + 1
+
+print("-----------")
+
+num = 0
+while num < 4:
+    print("hello python")
+    num = num + 1
+else:
+    print("bye python")
+
+print("---------------------")
+
+# for loop
+nums = [4, 7, 2, 9]
+for num in nums:
+    print(num)
+else:
+    print("Done!!")
+
+print("---------------------")
+
+alphabets = ['A', 'C', 'B', 'F']
+for index in range(len(alphabets)):
+    print(alphabets[index])
+else:
+    print("End!!")
+
+print("---------------------")
+
+for index in range (2, 5):
+    print("A" + str(index))
