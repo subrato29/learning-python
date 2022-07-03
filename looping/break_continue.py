b9b2c1c625e6dbd3d3b0381699f16b56d810ea39
subrato29 <subratosarkar29@gmai.com>
2022-07-03 15:33:24 -0400

    adding break continue concept

---
+++ looping/break_continue.py
@@ -0,0 +1,16 @@
+name = "Subrato"
+for i in name:
+    print(i)
+    if i == 'r':
+        break
+
+print("----------------")
+
+name = "Subrato"
+for i in name:
+    print(i)
+    if i == 'r':
+        continue
+
+print("-----------------")
+program = ["java", "python", "javascript", "golang"]
