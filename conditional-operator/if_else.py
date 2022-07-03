332f69b29ca6c28cc69b87f5d1886c0d03d2e149
subrato29 <subratosarkar29@gmai.com>
2022-07-03 09:54:08 -0400

    conditional operator added

---
+++ conditional-operator/if_else.py
@@ -0,0 +1,13 @@
+num = -6
+
+if num > 0:
+    print("num is a positive number")
+elif num < 0:
+    print("num is a negative number")
+elif num == 0:
+    print("num is zero")
+else:
+    print("not defined")
+
+num = 20;
+print("The number is: " + str (num)) # we can't write concatenate integer directly with string, unlike Java
